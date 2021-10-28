30ac6ba6e55c790b35402bb47bd7b5d65c73fecc
Asaduzzaman <ajaman375@gmail.com>
2021-10-28 11:40:39 -0700

    serverRunning

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 5000;
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
 // MongoDB Connection 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mangomarketecom.gzttn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+app.get("/",(req,res)=>{
+    res.send("Server Running");
+})
+
+app.listen(process.env.PORT || port ,()=> {
+    console.log("Server Running on Port", process.env.PORT || port);
+})
