b54a478c15f639139f24573f0dbb13784a840592
Asaduzzaman <ajaman375@gmail.com>
2021-10-28 11:53:49 -0700

    heroku added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectId;
+const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -12,6 +13,36 @@ app.use(express.json());
 // MongoDB Connection 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mangomarketecom.gzttn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("egTravels");
+      const offerCollection = database.collection("offers");
+      
+      //Get Api
+      app.get('/offers', async(req, res)=>{
+          const cursor = offerCollection.find({});
+          const offers = await cursor.toArray();
+          res.json(offers);
+      })
+
+      // Get Single Service API
+
+      //POST API
+     app.post('/services', async(req, res)=>{
+         const service = req.body;
+         console.log("hit the post api" , service);
+         const result = await volentureCollection.insertOne(service); 
+         res.json(result)
+     })
+    
+    //   console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 
 app.get("/",(req,res)=>{
