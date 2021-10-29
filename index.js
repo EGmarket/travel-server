b40c2bb01a18b872ce69a3486f08d64ea90c8ff7
Asaduzzaman <ajaman375@gmail.com>
2021-10-29 12:47:42 -0700

    finanly worked

--- index.js
+++ index.js
@@ -10,123 +10,133 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Connection 
+// MongoDB Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mangomarketecom.gzttn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 async function run() {
-    try {
-      await client.connect();
-      const database = client.db("egTravels");
-      const offerCollection = database.collection("offers");
-      const honeyCollection = database.collection("honeyTours");
-      const popularCollection = database.collection("popularTours");
-      const topsCollection = database.collection("topTours");
-      const ordersCollection = database.collection("orders");
-      
-      //Get Api offer
-      app.get('/offers', async(req, res)=>{
-          const cursor = offerCollection.find({});
-          const offers = await cursor.toArray();
-          res.json(offers);
-      })
-      // Get API Honey
-      app.get('/honey', async(req, res)=>{
-          const cursor = honeyCollection.find({});
-          const honey = await cursor.toArray();
-          res.json(honey);
-      })
-      // Get API Popular
-      app.get('/popular', async(req, res)=>{
-          const cursor = popularCollection.find({});
-          const popular = await cursor.toArray();
-          res.json(popular);
-      })
+  try {
+    await client.connect();
+    const database = client.db("egTravels");
+    const offerCollection = database.collection("offers");
+    const honeyCollection = database.collection("honeyTours");
+    const popularCollection = database.collection("popularTours");
+    const topsCollection = database.collection("topTours");
+    const ordersCollection = database.collection("orders");
 
-      // Get Single Service API
-      /* ----------------------------POST-route-start-------------------------------------- */
-      //POST Honeymoon API
-      app.post('/honey', async(req, res) => {
-        const newUser = req.body;
-        const result = await honeyCollection.insertOne(newUser)
-        console.log("got new user", req.body);
-        console.log('Added user',result);
-        res.json(result)
-      });
-      //POST Offer/Service API
-      app.post('/offers', async(req, res) => {
-        const newUser = req.body;
-        const result = await offerCollection.insertOne(newUser)
-        console.log("got new user", req.body);
-        console.log('Added user',result);
-        res.json(result)
-      });
-      //POST Popular API
-      app.post('/populars', async(req, res) => {
-        const newUser = req.body;
-        const result = await popularCollection.insertOne(newUser)
-        console.log("got new user", req.body);
-        console.log('Added user',result);
-        res.json(result)
-      });
-      //POST TOP API
-      app.post('/tops', async(req, res) => {
-        const newUser = req.body;
-        const result = await topsCollection.insertOne(newUser)
-        console.log("got new user", req.body);
-        console.log('Added user',result);
-        res.json(result)
-      });
-      /* ---------------END-POST-ROUTE----------------------------------------------- */
-    
-      /* --------------GET-ORDER----------------------------------- */
-      // Order Insert
-      app.post('/orders', async(req, res) => {
-        const newOrder = req.body;
-        const result = await ordersCollection.insertOne(newOrder)
-        console.log("got new user", req.body);
-        console.log('Added user',result);
-        res.json(result)
-      });
+    //Get Api offer
+    app.get("/offers", async (req, res) => {
+      const cursor = offerCollection.find({});
+      const offers = await cursor.toArray();
+      res.json(offers);
+    });
+    // Get API Honey
+    app.get("/honey", async (req, res) => {
+      const cursor = honeyCollection.find({});
+      const honey = await cursor.toArray();
+      res.json(honey);
+    });
+    // Get API Popular
+    app.get("/popular", async (req, res) => {
+      const cursor = popularCollection.find({});
+      const popular = await cursor.toArray();
+      res.json(popular);
+    });
 
-      //  Get specific order
+    // Get Single Service API
+    /* ----------------------------POST-route-start-------------------------------------- */
+    //POST Honeymoon API
+    app.post("/honey", async (req, res) => {
+      const newUser = req.body;
+      const result = await honeyCollection.insertOne(newUser);
+      console.log("got new user", req.body);
+      console.log("Added user", result);
+      res.json(result);
+    });
+    //POST Offer/Service API
+    app.post("/offers", async (req, res) => {
+      const newUser = req.body;
+      const result = await offerCollection.insertOne(newUser);
+      console.log("got new user", req.body);
+      console.log("Added user", result);
+      res.json(result);
+    });
+    //POST Popular API
+    app.post("/populars", async (req, res) => {
+      const newUser = req.body;
+      const result = await popularCollection.insertOne(newUser);
+      console.log("got new user", req.body);
+      console.log("Added user", result);
+      res.json(result);
+    });
+    //POST TOP API
+    app.post("/tops", async (req, res) => {
+      const newUser = req.body;
+      const result = await topsCollection.insertOne(newUser);
+      console.log("got new user", req.body);
+      console.log("Added user", result);
+      res.json(result);
+    });
+    /* ---------------END-POST-ROUTE----------------------------------------------- */
 
-      app.get('/orders/:email', async(req, res)=>{
-        const cursor = ordersCollection.find({email: req.params.email});
-        const order = await cursor.toArray();
-        res.json(order);
-    })
+    /* --------------GET-ORDER----------------------------------- */
+    // Order Insert
+    app.post("/orders", async (req, res) => {
+      const newOrder = req.body;
+      const result = await ordersCollection.insertOne(newOrder);
+      console.log("got new user", req.body);
+      console.log("Added user", result);
+      res.json(result);
+    });
+
+    // Get Single product by using ID
+    app.get("/order", async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    app.get("/order/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.findOne(query);
+      console.log(id);
+      res.send(result);
+    });
+
+    //  Get specific order by using email
+    app.get("/orders", async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    app.get("/orders/:email", async (req, res) => {
+      const cursor = ordersCollection.find({ email: req.params.email });
+      const order = await cursor.toArray();
+      res.json(order);
+    });
 
     // get all orders
 
-    app.get('/orders', async(req, res)=>{
-      const cursor = ordersCollection.find({});
-      const order = await cursor.toArray();
-      res.json(order);
-  })
+   
 
-  /* ------------------UPDATE----------------------------------- */
+    /* ------------------UPDATE----------------------------------- */
 
-    app.get('/orders/:id', async(req, res) =>{
-    const id = req.params.id;
-    const query = {_id: ObjectId(id)}
-    const result = await ordersCollection.findOne(query);
-    console.log(id);
-    res.json(result);
-  })
-
-
-    } finally {
+    
+  } finally {
     //   await client.close();
-    }
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
 
-app.get("/",(req,res)=>{
-    res.send("Server Running");
-})
-
-app.listen(process.env.PORT || port ,()=> {
-    console.log("Server Running on Port", process.env.PORT || port);
-})
+app.listen(process.env.PORT || port, () => {
+  console.log("Server Running on Port", process.env.PORT || port);
+});
